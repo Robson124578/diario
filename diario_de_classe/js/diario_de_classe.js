2549cd850da6d98f3573f539850634df6502abe4
Robson124578 <38814333+Robson124578@users.noreply.github.com>
2018-11-05 02:02:49 -0200

    trabalho-dia-05/11/2018
    as alteração na criação da página anotações estão em andamento com a criação do elemento para salvara as anotações quase concluído.

--- diario_de_classe/js/diario_de_classe.js
+++ diario_de_classe/js/diario_de_classe.js
@@ -38,3 +38,31 @@ function botaoanotacoes() {
 		document.getElementById("adicionar").textContent = "Adicionar";		
 	}
 }
+
+/*******************************/
+/**função para Slavar anotação**/
+/*******************************/
+
+function salvarAnotacao() {
+	var conteudoAnotacao = document.getElementById("info").value;
+
+	var divPai = document.getElementById("idAnotacoesSalvas");
+	
+		var novaDiv= document.createElement("div");
+			novaDiv.className = "anotacao";
+			divPai.appendChild(novaDiv);
+
+		var titulodiv = document.createElement("h1");
+		// var titulo    = document.createTextNode("Data: ");
+		// 	titulodiv.appendChild(titulo);
+			novaDiv.appendChild(titulodiv);
+			mes = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+		var data = new Date();
+			titulodiv.innerHTML += data.getDate() + " de " + mes[data.getMonth()] + " de " + data.getFullYear() + ", às " + " " + data.getHours() + ":" + data.getMinutes() + ".";
+
+				
+		var containerAnotacao = document.createElement("p");
+			containerAnotacao.innerHTML = conteudoAnotacao;
+			containerAnotacao.className = "conteudo-texto-anotacoes";
+			novaDiv.appendChild(containerAnotacao);
+}
