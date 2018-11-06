4424a32fc93c1afbc18008bc1ca67275996ecf12
Robson124578 <38814333+Robson124578@users.noreply.github.com>
2018-11-06 17:08:15 -0200

    trabalho dia 06/11/2018
    terminha ações da pagina anotações

--- diario_de_classe/js/diario_de_classe.js
+++ diario_de_classe/js/diario_de_classe.js
@@ -44,25 +44,52 @@ function botaoanotacoes() {
 /*******************************/
 
 function salvarAnotacao() {
+	// variável que recebe o valor digitado no textarea
 	var conteudoAnotacao = document.getElementById("info").value;
 
+	// a divpai vai receber a nova div a ser criada
 	var divPai = document.getElementById("idAnotacoesSalvas");
-	
+		// criação da nova div
 		var novaDiv= document.createElement("div");
 			novaDiv.className = "anotacao";
-			divPai.appendChild(novaDiv);
-
-		var titulodiv = document.createElement("h1");
-		// var titulo    = document.createTextNode("Data: ");
-		// 	titulodiv.appendChild(titulo);
+			divPai.appendChild(novaDiv); 
+		//	
+		var titulodiv = document.createElement("h2");
+			titulodiv.className = "tituloNovaDiv";
 			novaDiv.appendChild(titulodiv);
 			mes = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+			//função para concatenar o '0' antes dos minutos.
+			function concatenarZero(numero) {
+				if (numero < 10) {
+					numero = "0" + numero;
+				}
+				return numero;
+			}
 		var data = new Date();
-			titulodiv.innerHTML += data.getDate() + " de " + mes[data.getMonth()] + " de " + data.getFullYear() + ", às " + " " + data.getHours() + ":" + data.getMinutes() + ".";
+			titulodiv.innerHTML += data.getDate() + " de " + mes[data.getMonth()] + " de " + data.getFullYear() + ", às " + " " + data.getHours() + ":" + concatenarZero(data.getMinutes()) + ".";
 
+		var botaoConfigAnotacao	= document.createElement("i");
+			botaoConfigAnotacao.className = "fas fa-ellipsis-v";
+			botaoConfigAnotacao.setAttribute('onclick','opcoes()');
+			novaDiv.appendChild(botaoConfigAnotacao);
 				
 		var containerAnotacao = document.createElement("p");
 			containerAnotacao.innerHTML = conteudoAnotacao;
 			containerAnotacao.className = "conteudo-texto-anotacoes";
 			novaDiv.appendChild(containerAnotacao);
-}
+
+		var botaoCrud = document.createElement("button");
+			botaoCrud.innerHTML = "editar";
+			botaoCrud.setAttribute('type','button');
+			botaoCrud.className = "sumir"
+			novaDiv.appendChild(botaoCrud);
+
+		var botaoCrud = document.createElement("button");
+			botaoCrud.innerHTML = "excluir";
+			botaoCrud.setAttribute('type','button');
+			botaoCrud.className = "sumir"
+			novaDiv.appendChild(botaoCrud);			
+}	
+	function opcoes() {
+		var aparcer = document.getElementsByClassName("sumir").classList.toggle("aparecer");
+	}
